8aecbec3a837efa5021cc16bac8683d9407a4e7a
William Czubakowski <williamczuba@gmail.com>
2024-02-15 13:25:56 -0800

    wip: try to fix workflow some more

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,14 @@
 import {
   awscdk, javascript
 } from 'projen';
-import { JobPermission } from 'projen/lib/github/workflows-model';
+import { Job } from 'projen/lib/github/workflows-model';
 
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'William Czubakowski',
   authorAddress: 'williamczuba@gmail.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.0.0',
+  jsiiVersion: '~5.2.0',
   name: 'parent-child-pipelines',
   projenrcTs: true,
   repositoryUrl: 'git@github.com:angellist/parent-child-pipelines.git',
@@ -40,6 +40,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@aws-sdk/client-codepipeline',
     'aws-cdk-lib@^2.96.0',
   ],
+  peerDeps: [
+    'aws-cdk-lib@^2.96.0',
+  ],
   bundledDeps: [
     '@aws-sdk/credential-providers',
     '@aws-sdk/client-codepipeline',
@@ -61,38 +64,70 @@ project.addScripts({
   'execute-child-pipelines': 'npx ts-node src/execute-child-pipelines/index.ts',
 });
 const workflow = project.github!.workflows.find((wf) => wf.name === 'build')!;
-// new job just to install npm and bun that we'll require to run before the build
-// const setupNodeStep = {
-//   name: 'Setup Node.js',
-//   uses: 'actions/setup-node@v4',
-//   with: {
-//     'node-version': '18.x',
-//   },
-// };
-// const installBunStep = {
-//   name: 'Install bun',
-//   run: 'npm i -g bun',
-// };
 
-const standaloneInstallBun = {
-  name: 'Install bun',
-  run: 'curl https://bun.sh/install | bash',
-};
+const existingJob = workflow.getJob('build')! as Job;
 
-const installDependenciesJob = {
-  runsOn: ['ubuntu-latest'],
-  steps: [standaloneInstallBun],
-  permissions: {
-    contents: JobPermission.WRITE,
-  }
-};
-
-workflow.addJob('install-dependencies', installDependenciesJob);
-
-const existingJob = workflow.getJob('build')!
 workflow.updateJob('build', {
   ...existingJob,
-  needs: ['install-dependencies'],
+  steps: [
+    {
+      name: 'Checkout',
+      uses: 'actions/checkout@v4',
+      with: {
+        ref: '${{ github.event.pull_request.head.ref }}',
+        repository: '${{ github.event.pull_request.head.repo.full_name }}'
+      }
+    },
+    {
+      name: 'Setup Node.js',
+      uses: 'actions/setup-node@v4',
+      with: {
+        'node-version': '18.x'
+      }
+    },
+    {
+      name: 'Install dependencies',
+      run: 'npm install -g bun && bun install'
+    },
+    {
+      name: 'build',
+      run: 'npx projen build'
+    },
+    {
+      name: 'Find mutations',
+      id: 'self_mutation',
+      run: `git add . && git diff --staged --patch --exit-code > .repo.patch || echo "self_mutation_happened=true" >> $GITHUB_ENV`
+    },
+    {
+      name: 'Upload patch',
+      if: 'steps.self_mutation.outputs.self_mutation_happened',
+      uses: 'actions/upload-artifact@v4',
+      with: {
+        name: '.repo.patch',
+        path: '.repo.patch',
+        overwrite: 'true'
+      }
+    },
+    {
+      name: 'Fail build on mutation',
+      if: 'steps.self_mutation.outputs.self_mutation_happened',
+      run: `echo "::error::Files were changed during build (see build log). If this was triggered from a fork, you will need to update your branch." && cat .repo.patch && exit 1`
+    },
+    {
+      name: 'Backup artifact permissions',
+      run: 'cd dist && getfacl -R . > permissions-backup.acl',
+      continueOnError: true
+    },
+    {
+      name: 'Upload artifact',
+      uses: 'actions/upload-artifact@v4',
+      with: {
+        name: 'build-artifact',
+        path: 'dist',
+        overwrite: 'true'
+      }
+    }
+  ]
 });
 
 
