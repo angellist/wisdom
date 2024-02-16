20097a5b0b77092f766fadbf15921f80f8147c07
William Czubakowski <williamczuba@gmail.com>
2024-02-16 11:18:34 -0800

    fix: type

--- .projenrc.ts
+++ .projenrc.ts
@@ -56,7 +56,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   githubOptions: {
     mergify: false,
-  }
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
